57d02af2e509b3002a660c9d4d28972bf2748ad3
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-08-25 23:11:08 -0300

    fixed publishing for @xethya/point

--- packages/point/rollup.config.js
+++ packages/point/rollup.config.js
@@ -1,43 +1,42 @@
-import typescript from 'rollup-plugin-typescript2';
-import json from 'rollup-plugin-json';
+import json from "rollup-plugin-json";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
-const globals = {};
-
-import pkg from './package.json';
+const globals = {
+  "@xethya/utils": "xethya.utils",
+  uuid: "uuid",
+};
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: pkg.main,
         sourcemap: true,
-        format: 'cjs',
-        globals: globals
+        format: "cjs",
+        globals: globals,
       },
       {
         file: pkg.module,
         sourcemap: true,
-        format: 'es',
-        globals: globals
+        format: "es",
+        globals: globals,
       },
       {
         file: pkg.iife,
         sourcemap: true,
-        format: 'iife',
-        name: 'xethya.' + pkg.shortName,
-        globals: globals
-      }
+        format: "iife",
+        name: "xethya." + pkg.shortName,
+        globals: globals,
+      },
     ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {})
-    ],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     plugins: [
       typescript({
-        typescript: require('typescript')
+        typescript: require("typescript"),
       }),
-      json({ compact: true })
-    ]
-  }
+      json({ compact: true }),
+    ],
+  },
 ];
