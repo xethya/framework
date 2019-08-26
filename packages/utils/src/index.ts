6beafb33a66b7bf35dcff3540c3c80b13e79b9b5
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-08-25 22:25:48 -0300

    exported new modules

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,17 +1,11 @@
-import * as array from './array';
-import assert from './assert';
-import Collection from './collection';
-import * as numeric from './numeric';
-import Range from './range';
-import * as string from './string';
-import * as Time from './time';
+import * as array from "./array";
+import assert from "./assert";
+import Collection from "./collection";
+import * as Node from "./node";
+import * as numeric from "./numeric";
+import Range from "./range";
+import { Stack } from "./stack";
+import * as string from "./string";
+import * as Time from "./time";
 
-export {
-  array,
-  assert,
-  Collection,
-  numeric,
-  string,
-  Range,
-  Time,
-};
+export { array, assert, Collection, numeric, Node, Stack, string, Range, Time };
