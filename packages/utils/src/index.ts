f4b7437cee2eee93f14de442fb5d75c4553e1d08
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-14 12:43:51 -0300

    exporting everything from node, stack, types

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,11 +1,12 @@
 import * as array from "./array";
 import assert from "./assert";
 import Collection from "./collection";
-import * as Node from "./node";
 import * as numeric from "./numeric";
 import Range from "./range";
-import { Stack } from "./stack";
 import * as string from "./string";
 import * as Time from "./time";
 
-export { array, assert, Collection, numeric, Node, Stack, string, Range, Time };
+export * from "./node";
+export * from "./stack";
+export * from "./types";
+export { array, assert, Collection, numeric, string, Range, Time };
