a462412a085b203457dcfb8b6c5b4fd734ae488f
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-21 12:46:37 -0300

    implemented Phaser declarations for Xethya

--- packages/bridge-phaser/src/index.ts
+++ packages/bridge-phaser/src/index.ts
@@ -1,3 +1,5 @@
+import { Die } from "@xethya/dice";
+import { Inventory, InventoryOptions } from "@xethya/inventory";
 import { Plugins } from "phaser";
 import { dieCreator, dieCreatorFor, dieFactory, dieFactoryFor } from "./factories/dice";
 import { inventoryCreator, inventoryFactory } from "./factories/inventory";
@@ -19,5 +21,30 @@ export class XethyaPlugin extends Plugins.BasePlugin {
 
     // Register @xethya/inventory
     pluginManager.registerGameObject("inventory", inventoryFactory, inventoryCreator);
+  }
+}
+
+declare module "phaser" {
+  export namespace GameObjects {
+    export interface InventoryGameObject extends GameObject {
+      inventory: Inventory;
+    }
+
+    export interface DieGameObject extends GameObject {
+      die: Die;
+    }
+
+    export interface GameObjectFactory {
+      inventory(options?: InventoryOptions): InventoryGameObject;
+      die(faces?: number): DieGameObject;
+      coinFlip(): DieGameObject;
+      d4(): DieGameObject;
+      d6(): DieGameObject;
+      d8(): DieGameObject;
+      d10(): DieGameObject;
+      d12(): DieGameObject;
+      d20(): DieGameObject;
+      d100(): DieGameObject;
+    }
   }
 }
