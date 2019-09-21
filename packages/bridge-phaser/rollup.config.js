e7bda477312543c27efd76cfa1faa8a910aff9b7
Joel Alejandro Villarreal Bertoldi <joel.a.villarreal@gmail.com>
2019-09-21 03:08:19 -0300

    bumped bridge-phaser to 0.0.2, makes it usable (#84)

---
+++ packages/bridge-phaser/rollup.config.js
@@ -0,0 +1,43 @@
+import json from "rollup-plugin-json";
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+const globals = {
+  phaser: "Phaser",
+  "@xethya/dice": "xethya.dice",
+  "@xethya/inventory": "xethya.inventory",
+};
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        sourcemap: true,
+        format: "cjs",
+        globals: globals,
+      },
+      {
+        file: pkg.module,
+        sourcemap: true,
+        format: "es",
+        globals: globals,
+      },
+      {
+        file: pkg.iife,
+        sourcemap: true,
+        format: "iife",
+        name: "xethya." + pkg.shortName,
+        globals: globals,
+      },
+    ],
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      typescript({
+        typescript: require("typescript"),
+      }),
+      json({ compact: true }),
+    ],
+  },
+];
